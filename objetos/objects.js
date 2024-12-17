9a6324a73a094b50eace0bd4bd2eef8fe576bcbd
DarkJutsu <ulloajose34@gmail.com>
2024-12-16 21:17:52 -0600

    feat(objetos/):Add type objects

---
+++ objetos/objects.js
@@ -0,0 +1,18 @@
+"use strict";
+(() => {
+    /* EL SIMBOLO ? ESTABLECE QUE UN PARA METRO ES OPCIONAL */
+    let flash = {
+        name: 'Barry allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo'],
+    };
+    flash = {
+        name: 'Clark Kent',
+        /* age: 60, */
+        powers: ['Super fuerza'],
+        /* getNombre() {
+          return this.name;
+        }, */
+    };
+    console.log(flash);
+})();
---
+++ objetos/objects.ts
@@ -0,0 +1,19 @@
+(() => {
+  /* EL SIMBOLO ? ESTABLECE QUE UN PARA METRO ES OPCIONAL */
+  let flash: { name: string; age?: number; powers: string[] } = {
+    name: 'Barry allen',
+    age: 24,
+    powers: ['Super velocidad', 'Viajar en el tiempo'],
+  };
+
+  flash = {
+    name: 'Clark Kent',
+    /* age: 60, */
+    powers: ['Super fuerza'],
+    /* getNombre() {
+      return this.name;
+    }, */
+  };
+
+  console.log(flash);
+})();
