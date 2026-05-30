37730053ea176225695d787bed8884128df3f58e
DarkJutsu <ulloajose34@gmail.com>
2026-05-29 19:22:05 -0600

    feat:add calculator functions and main module for demonstration

---
+++ module/calculator.ts
@@ -0,0 +1,17 @@
+export function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+export function rest(num1: number, num2: number): number {
+  return num1 - num2;
+}
+
+export function multiple(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+export function div(num1: number, num2: number): number {
+  return num1 / num2;
+}
+
+export const PI: number = 3.1416;
---
+++ module/main.ts
@@ -0,0 +1,5 @@
+import { PI, sum } from './calculator';
+
+console.log(`PI: ${PI}`);
+
+console.log(sum(12, 23));
